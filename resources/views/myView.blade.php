552a0f073c766be139608d8355466fdb7b696afe
paolotrimini <paolo.trimini@gmail.com>
2021-05-15 16:04:10 +0200

    push codice

---
+++ resources/views/myView.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- VueJS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <!-- Bootstrap (CSS) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>LARAVEL - Primi passi</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+</head>
+<body>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="box-title">
+                    <h1>Hello World!</h1>
+                </div>
+                <hr>
+                <div class="box-subtitle">
+                    <h2>Laravel primi passi</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
